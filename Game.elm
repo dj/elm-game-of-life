1aa5aa71048c847c9f7e3a134d4c95d4d2344649
DJ Hartman <danieljayhartman@gmail.com>
2015-12-22 01:16:24 -0800

    Having some trouble with representing the game as an array, making the model simpler by just keeping track of the list of alive cells.

--- Game.elm
+++ Game.elm
@@ -1,61 +1,85 @@
-module Game where
+module Game  where
 
 import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
-import StartApp.Simple as StartApp 
-import Signal exposing (Address)
-import Debug
+-- import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
+import Svg
+import Svg.Attributes exposing (..)
+import StartApp.Simple as App
+
+
+-- MODEL
+
+type alias Cell =
+  { x: Int,
+    y: Int
+  }
+
+type alias Model = 
+  { step: Int,
+    size: Int,
+    living: List Cell
+  }
 
 -- UPDATE
 
-type Action =
-  NoOp | Step
+type Action = NoOp | Step
 
 
-update action model = 
+update : Action -> Model -> Model
+update action model =
   case action of
     NoOp ->
       model
     Step ->
       { model | step = model.step + 1 }
 
+
 -- VIEW
-eachRow address row =
-  div [ ]
-    [ row
-      |> toString
-      |> text
-    ]
+cellSize = 50
+
+-- drawCell : Cell -> Svg.Svg
+drawCell cell =
+  Svg.rect 
+    [ x (toString (cell.x * cellSize)), 
+      y (toString (cell.y * cellSize)), 
+      width (toString cellSize),
+      height (toString cellSize),
+      style "fill: #efefef"
+    ] []
+
+-- drawBoard : Int -> List Cell -> Svg.Svg
+drawBoard size living =
+  Svg.svg
+    [ width (toString (cellSize * size)), height (toString (cellSize * size)), style "background-color: #222" ]
+    (List.map drawCell living)
 
 
-board address model =
-  div [ ] 
-    (List.map (eachRow address) model.board)
-
+view : Signal.Address Action -> Model -> Html
 view address model =
-  div [ ]
-    [ board address model,
-      button [ onClick address Step ] [(text "step")],
-      text (toString model.step)
-    ]
-
-
--- MODEL
-initialModel =
-  { board = [ [0, 0, 0, 0],
-              [0, 0, 0, 0],
-              [0, 0, 0, 0],
-              [0, 0, 0, 0]
-            ],
-    step = 0
-  }
-
+  div []
+  [ text "Game of Life",
+    div []
+      [ drawBoard model.size model.living ],
+    div [] 
+      [ text (toString model.step) ],
+    div []
+      [ button [ onClick address Step ] [ text "Step" ] ]
+  ]
 
 main =
-  StartApp.start
-    { model = initialModel,
-      view = view,
-      update = update
+  App.start 
+    { view = view, 
+      update = update,
+      model = 
+        { step = 0,
+          size = 4,
+          living = 
+            [ { x = 0, y = 0 },
+              { x = 1, y = 1 },
+              { x = 2, y = 2 },
+              { x = 3, y = 3 }
+            ]
+        }
     }
 
